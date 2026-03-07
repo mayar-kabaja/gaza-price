ce9637c048a060f2d68ffb3107a00326a7fa2715
Mayar Kabaja <kabajamayar@email.com>
2026-03-07 23:58:24 +0200

    Update Sentry DSN for client, server, and edge configurations
    - Changed the Sentry DSN in sentry.client.config.ts, sentry.edge.config.ts, and sentry.server.config.ts to a new endpoint for improved error tracking and monitoring.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://39856ac9898e89cc04f2024cb219e61b@o1247021.ingest.us.sentry.io/4511005395320832",
+  dsn: "https://d1397d817341389c453475bd20f35e62@o1247021.ingest.us.sentry.io/4511005444931584",
   tracesSampleRate: 0.3,
   environment: process.env.NODE_ENV,
 });
